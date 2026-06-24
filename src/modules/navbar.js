0246c26501cf6bfa35643f4182435d63b7c73435
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-06-24 23:24:46 +0600

    style: refine hireBtn size

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -19,7 +19,7 @@ export function renderNavbar() {
       </ul>
       <a id='hireBtn'
         href="#contact"
-        class="hidden text-sm md:inline-block bg-accent text-bg px-4 py-2 rounded-lg hover:bg-accent-dim shadow-glow"
+        class="hidden text-[.85rem] font-medium md:inline-block bg-accent text-bg px-3.5 py-1.5 rounded-lg hover:bg-accent-dim shadow-glow"
         >Hire me</a
       >
      
